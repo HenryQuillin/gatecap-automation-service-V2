bc9b79e8ed3a136ffe41b5796a99a7e23f5c63a8
Henry Quillin <henryquillin@gmail.com>
2023-07-04 15:42:43 -0500

    working

--- getReport.js
+++ getReport.js
@@ -1,10 +1,24 @@
 require("dotenv").config();
+const _ = require('lodash');
+
 
 const axios = require("axios");
 const csv = require("csv-parser");
 const { Configuration, OpenAIApi } = require("openai");
 
 // eslint-disable-next-line no-undef
+
+
+// store the entire for investor report data in a json object 
+// For each company
+  // filter by that company 
+  // set the prompt to be the company name and the filtered data 
+    // note, the data should be truncated to 15,000 characters
+  // run the summarizeArticles function on the prompt
+  
+
+
+
 
 const configuration = new Configuration({
   apiKey: process.env.GPT_API_KEY,
@@ -13,58 +27,95 @@ const openai = new OpenAIApi(configuration);
 
 async function getArticleData() {
   try {
-    // const response = await axios.get('https://api.csvgetter.com/files/f0bc117df8?type=csv');
-    const response =
-      "Title,Company,Type,Content Preview,Link,Source,Alert Query String,Alert Email URL,Date,CreatedCrypto News: 🟪 Broken precedent,Prime Trust,blogs,'- Read more*Sponsored contentTop StoriesCME Group to add ether/bitcoin ratio futures in July pending regulatory approval —  Read   Emerging Prime Trust ties have traders fretting over TUSD — ReadBlockchain Association says legally, Gensler has...',https://bitcointrader2022.blogspot.com/2023/06/broken-precedent.html,bitcointrader2022.blogspot.com,'PrimeTrust OR ''Prime Trust''',https://mail.google.com/mail/u/3/#inbox/1890a08b9458fd2e,2023-06-29,2023-06-30ApeCoin DAO Votes In Two New Special Council Seats Replacing Alexis Ohanian and Yat Siu,Prime Trust,blogs,'...Crypto NinjaPost navigationPrevious Prime Trust Is Having a Bad...',https://cryptonewsday.wordpress.com/2023/06/30/apecoin-dao-votes-in-two-new-special-council-seats-replacing-alexis-ohanian-and-yat-siu/,cryptonewsday.wordpress.com,'PrimeTrust OR ''Prime Trust''',https://mail.google.com/mail/u/3/#inbox/1890942cfc666d58,2023-06-29,2023-06-29Coinbits Hit by Prime Trust Regulatory Freeze,Prime Trust,news,'...asset platform with a relationship with Prime Trust – now insolvent – that finds that some customers cannot access their ... directly to Bitcoin Twitter about what the Prime Trust meltdown means for Coinbits.— Coinbits | DCA into bitcoin...',https://www.crowdfundinsider.com/2023/06/209452-coinbits-hit-by-prime-trust-regulatory-freeze/,crowdfundinsider.com,'PrimeTrust OR ''Prime Trust''',https://mail.google.com/mail/u/3/#inbox/1890942cfc666d58,2023-06-29,2023-06-29Emerging Prime Trust ties have traders fretting over TUSD,Prime Trust,news,'...or TUSD assets reside with Prime Trust. Representatives for Prime Trust and TUSD did not return requests for comment or... Wait, the auditor who has been attesting to the $TUSD audits (in Prime Trust) was the old FTX auditor who set up under a...','https://cryptonews.net/news/altcoins/21250952/, https://ro.bitcoinethereumnews.com/tech/emerging-prime-trust-ties-have-traders-fretting-over-tusd/','cryptonews.net, ro.bitcoinethereumnews.com','PrimeTrust OR ''Prime Trust''',https://mail.google.com/mail/u/3/#inbox/1890942cfc666d58,2023-06-29,2023-06-29Prime Trust Is Having a Bad Month,Prime Trust,news,'Prime Trust may be taken over by the state of Nevada, and the situation seems alarming.','https://www.15minutenews.com/article/2023/06/29/231054454/prime-trust-is-having-a-bad-month/, https://www.newslocker.com/en-us/news/cryptocurrency/prime-trust-is-having-a-bad-month/, https://eleven87.blog/2023/06/29/prime-trust-is-having-a-bad-month/','15minutenews.com, newslocker.com, eleven87.blog','PrimeTrust OR ''Prime Trust''',https://mail.google.com/mail/u/3/#inbox/1890942cfc666d58,2023-06-29,2023-06-29'LayerZero’s market shifting, there’s a better environment ahead for everyone, CEO says',Prime Trust,news,'...LayerZero provides for it.";
-    // let data = [];
-    // response.data
-    //     .pipe(csv())
-    //     .on('data', (row) => {
-    //         data.push(row);
-    //     })
-    //     .on('end', () => {
-    //         console.log('CSV successfully processed');
-    //     });
-    // return data;
-    return response;
+    const json = await axios.get('https://api.csvgetter.com/files/f0bc117df8?type=json_records&cols=Title,Company,Content Preview,Links');
+    const res = json.data;
+    return res;
   } catch (error) {
     console.error(error);
   }
 }
 
-async function summarizeArticles(csvData) {
-  // let csvAsString = JSON.stringify(csvData);
+
+
+
+
+async function summarizeArticles(articleData, companyName) {
+  // let articles = articleData.map(article => `${article.Title}: ${article['Content Preview']}`).join("\n");
+  let articles = JSON.stringify(articleData, null, 2);
   let prompt =
-    "Here is csv data of the news log of my venture capital firm. It contains the title, snippet with mentioned portfolio company, and relevant portfolio company of a collection of articles gathered from the past week. Can you summarize this into an investor update?\n" +
-    csvData;
+  `As an AI developed by OpenAI, you have been tasked with assisting in the monitoring of our current portfolio companies. Specifically, your role is to digest and summarize the key events and news related to our portfolio company, ${companyName}, over the past week.
+
+The audience for this summary is a senior executive who will use your summary to write an investor report. They already have a deep understanding of the company, so you do not need to explain what the company does or provide any analysis or recommendations. Your task is purely to summarize the recent events in a comprehensive manner.
+
+Please generate an HTML formatted summary of the key events from these articles in the format (use <ul>/<li> list with 'subject: details' format for the list of events):
+  <h2>${companyName}</h2>
+ 
+  <ul>
+    <li><strong>Subject</strong>: Details</li>
+    ...
+    <!-- Include the rest of the individual summaries of the events here -->
+  </ul> 
+
+Here is the collected news data related to ${companyName} for the past week:
+${articles}
+
+Please provide an exhaustive and comprehensive summary of the key events from these articles. Include source links where necessary`;
+
+
+
 
   try {
+    console.log("generating summmary for: ", companyName);
     const response = await openai.createChatCompletion({
       messages: [
-        { role: "system", content:prompt },
-        { role: "user", content: "Say this is a test" },
+        { role: "system", content:"You are a helpful AI assistant" },
+        { role: "user", content: prompt },
       ],
       model: "gpt-3.5-turbo-16k",
     });
-
+    console.log("summary generated for: ", companyName);
     return response.data.choices[0].message.content;
   } catch (error) {
+        console.log("summary generated for: ", companyName);
+
     console.error(error);
   }
 }
+function formatReports(summaries) {
+  let mergedReports = '<!DOCTYPE html>\n<html>\n<head>\n<style>\nbody {\nfont-family: Arial, sans-serif;\n}\n\nh1 {\ncolor: #333333;\nfont-size: 24px;\nfont-weight: bold;\nmargin-bottom: 10px;\n}\n\np {\ncolor: #666666;\nfont-size: 16px;\nline-height: 1.5;\n}\n</style>\n</head>\n<body>';
+
+  for (let company in summaries) {
+    mergedReports += summaries[company];
+  }
+
+  mergedReports += '\n</body>\n</html>';
+
+  return mergedReports;
+}
+
+
+
 
 async function getReport(req, res) {
   try {
-    const csvData = await getArticleData();
-    const summary = await summarizeArticles(csvData);
-    console.log(summary);
-    res.send(summary);
-    // Here you would call sendEmail(summary)
+    const articlesData = await getArticleData();
+    const groupedByCompany = _.groupBy(articlesData, "Company"); // Group the data by Company field
+
+    let reports = {};
+
+    // Iterate over each group
+    for (let company in groupedByCompany) {
+      const summary = await summarizeArticles(groupedByCompany[company],company);
+      reports[company] = summary;
+      // Here you can call sendEmail(summary, company) to send a report for each company separately
+    }
+
+    const formattedReport = formatReports(reports);
+    res.send(formattedReport); // send the reports as the response
   } catch (error) {
     console.error(error);
   }
 }
 
-module.exports = { getReport };
 
-// https://api.csvgetter.com/files/f0bc117df8?type=csv
+module.exports = { getReport };
